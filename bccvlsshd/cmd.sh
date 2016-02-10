c981bbe47c5266ff3320bbf56ce678ba8df9b1e9
Gerhard Weis <g.weis@griffith.edu.au>
2016-02-10 12:18:18 +1000

    update bccvlsshd container to changes in zope based containers make sure ssh related files have correct permissions

--- bccvlsshd/cmd.sh
+++ bccvlsshd/cmd.sh
@@ -2,4 +2,14 @@
 
 /usr/sbin/sshd-keygen
 
+
+# COPY pub keys from /etc/opt/zope/zope.id_rsa.pub to /opt/zope/.ssh/authorized_keys to ensure correct permissions
+
+mkdir -p $Z_HOME/.ssh
+cp $Z_CONF/zope.id_rsa.pub $Z_HOME/.ssh/authorized_keys
+
+chown -R zope:zope $Z_HOME
+chmod 500 $Z_HOME/.ssh
+chmod 600 $Z_HOME/.ssh/*
+
 exec /usr/sbin/sshd -D -e
