878218c05c89df910cd38c77f9de02358afda67a
Gerhard Weis <g.weis@griffith.edu.au>
2016-08-25 11:07:14 +1000

    use custom zope.conf in bccvl container fix path to copy out generated zope.conf

--- scripts/buildout.sh
+++ scripts/buildout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker-compose run --name bccvl_buildout bccvl ./bin/buildout -c compose.cfg
-docker cp bccvl_buildout:/opt/bccvl/parts/instance/zope.conf ./zope.conf
+docker cp bccvl_buildout:/opt/bccvl/parts/instance/etc/zope.conf ./zope.conf
 docker rm bccvl_buildout
